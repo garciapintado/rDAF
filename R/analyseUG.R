cbaa1189e89123e2df11483a7cf72e082aaff1eb
garciapintado <jgarciapintado@gmail.com>
2018-12-05 15:46:53 +0100

    removed SpatialGraph dependency

--- R/analyseUG.R
+++ R/analyseUG.R
@@ -51,6 +51,9 @@ analyseUG <- function(G=NULL, gLON=NULL, gLAT=NULL, fit, prm, X, yls=NULL, gauDA
   options(warn=2)
   if (lite)
     mpi <- FALSE
+
+  if (!("package:SpatialGraph" %in% search()))
+    pointsToLines <- function(a,b) {stop('analyseUG: SpatialGraph not loaded')}
   
   if (!is.null(G) && !is.null(gLON))
     stop('analyseUG: use just either G [regular grids] or gLON,gLAT [irregular grids] for horizontal grid definition')

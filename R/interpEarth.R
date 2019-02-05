5b88b55f85c843a3def058fe5d570d654230af7b
garciapintado <jgarciapintado@gmail.com>
2019-02-05 14:56:18 +0100

    added patchwin to interpEarth

--- R/interpEarth.R
+++ R/interpEarth.R
@@ -1,4 +1,4 @@
-interpEarth <- function(LON, LAT, z, xo, yo, output = 'grid', retnum = TRUE) {
+interpEarth <- function(LON, LAT, z, xo, yo, output = 'grid', retnum = TRUE, patchwin=90) {
   # irregular points onto regular grid around the Earth by use of akima interpolation
   # require(interp)
 
@@ -12,39 +12,36 @@ interpEarth <- function(LON, LAT, z, xo, yo, output = 'grid', retnum = TRUE) {
     stop('interpEarth: output longitudes expand more than 360 degrees')
   if (!(output %in% c('grid','points')))
     stop('interpEarth: output must be grid or points')
+
+  if (any(diff(xo) <= 0))
+    stop('xo not monotonically increasing')
+  if (any(diff(yo) <= 0))
+    stop('yo not monotonically increasing')
   
-  # express input and output longitudes in [0,360]
+  # express input and output longitudes centered at 180 (in [0,360])
   LON360 <- LON
-  if (any(LON360 < 0))                                     # input in [-180,0)
-    LON360[LON360 < 0] <- LON360[LON360 < 0] + 360         # in [0,360]  
+  if (any(LON < 0))                                     # input in [-180,0)
+    LON360[LON < 0] <- LON[LON < 0] + 360               # in [0,360]  
 
   xo360 <- xo
-  if (any(xo360 < 0))                                      # output in [-180,0)
-    xo360[xo360 < 0]   <- xo360[xo360 < 0] + 360           # in [0,360]  
+  if (any(xo < 0))                                     # output in [-180,0)
+    xo360[xo < 0] <- xo[xo < 0] + 360                  # in [0,360]  
+  
+  # recycle patchwin at each side
+  lid <- which(LON360 > (360 - patchwin))  # patch to the left
+  rid <- which(LON360 < patchwin)          # patch to the right
+  LONaug <- c(LON360[lid]-360, LON360, LON360[rid]+360)
+  LATaug <- c(LAT[lid],        LAT,    LAT[rid]) 
+  zaug   <- c(z[lid],          z,      z[rid])
 
-  
-  # augment source data by duplicating points for ranges [-90,0] & [360,450]  
-  LONaug <- LON
-  LATaug <- LAT
-  zaug   <- z
-  
-  ldid   <- which(LON360 > 180)                            # left duplicated indices
-  LONaug <- c(LONaug,LON360[ldid]-360)
-  LATaug <- c(LATaug,LAT[ldid])
-  zaug   <- c(zaug,zaug[ldid])
-
-  rdid   <- which(LON360 < 180)                            # right duplicated indices
-  LONaug <- c(LONaug,LON360[rdid]+360)
-  LATaug <- c(LATaug,LAT[rdid])
-  zaug   <- c(zaug,zaug[rdid])
-  
   if (output != 'grid')
     zo <- interp::interp(LONaug, LATaug, zaug, xo=xo360, yo=yo, output=output)$z
   else {
-   # if (requireNamespace("akima")) 
-      zo <- akima::interp(LONaug, LATaug, zaug, xo=xo360, yo=yo)$z  # much faster
-   # else
-   #   stop('akima::interp not available')
+    zo <- matrix(NA,length(xo),length(yo))
+    if (any(xo < 0))
+      zo[xo <  0,] <-  akima::interp(LONaug, LATaug, zaug, xo=xo[xo < 0]+360, yo=yo)$z
+    if (any(xo >= 0))
+      zo[xo >= 0,] <-  akima::interp(LONaug, LATaug, zaug, xo=xo[xo >= 0], yo=yo)$z
   }  
   if (output == 'grid' && retnum)                            # write as numeric from ul corner and advancing by row=TRUE
     zo <- as.numeric(zo[,ncol(zo):1])
